217de022498452488b3970c2b9b291f73fd9ac93
Diamond <Diamond@10.211.55.5>
2018-05-24 22:49:39 +0300

    Task A: handShake! Call functions in necessary sequence to organize dialog between Guest and Visitor

---
+++ Test/src/Tasks/A/guest.java
@@ -0,0 +1,7 @@
+package Tasks.A;
+
+public class guest {
+	void handShakeWithVisitor(){
+		System.out.println("Hi dear, \nnice to see you. I am a new guest here.");
+	}
+}
---
+++ Test/src/Tasks/A/visitor.java
@@ -0,0 +1,7 @@
+package Tasks.A;
+
+public class visitor {
+	void handShakeWithGuest(){
+		System.out.println("Hi mate, \nnice to see you as well. Welcome!.");
+	}
+}
