93ca1a6fb567677d429851aac358d40e48727548
The Sun <The Sun@DESKTOP-25V8A43>
2018-06-17 23:35:39 +0300

    task 2 done

--- Test/src/task/b/RunMassive.java
+++ Test/src/task/b/RunMassive.java
@@ -7,17 +7,14 @@ public class RunMassive {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-			int[] arrayExample = new int [5];
-			arrayExample[0] = 1;
-			arrayExample[1] = 45;
-			arrayExample[2] = 23;
-			arrayExample[3] = 68;
-			arrayExample[4] = 6;
+			int[] arrayExample = new int [MathHelper.random(1, 100)];
 						
+			for (int i=0; i <= arrayExample.length-1; i ++ ) {
 			
-			for (int i=0; i < 1000; i ++ ) {
+				arrayExample[i] = MathHelper.random(10, 1000);
+							
+				System.out.println(arrayExample[i]);
 			
-				System.out.println(MathHelper.random(5, 49));
 			}
 			
 			
