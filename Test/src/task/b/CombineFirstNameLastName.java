5e5687f31551eb80245fbe921431200c5c48dbdb
veron <veron@DESKTOP-L5541OT>
2018-08-13 22:42:13 +0300

    column

--- Test/src/task/b/CombineFirstNameLastName.java
+++ Test/src/task/b/CombineFirstNameLastName.java
@@ -7,101 +7,111 @@ import task.utilities.MathHelper;
 import task.utilities.MathMultiHelper;
 
 public class CombineFirstNameLastName {
-	private static final int arrayLength = 5;
+	private static final int NUMBER_FIRST_NAME_GENERATED = 10;
+	private static final int NUMBER_PATRONYMIC_GENERATED = 8;
+	private static final int NUMBER_LAST_NAME_GENERATED = 7;
+	private static final int NUMBER_PEOPLE_GENERATED = 4;
 	public static void main(String[] args) {
-		String [] arrayFirstName = doGenerateFirstNameArray();
-		for(int i=0; i< arrayFirstName.length-1; i++) {
-			System.out.println(arrayFirstName[i]);
-		}
-		String [] arrayLastName = doGenerateLastNameArray();
-		for(int i=0; i< arrayLastName.length-1; i++) {
-			System.out.println(arrayLastName[i]);
-		}	
-		String [] arraySecondName = doGenerateSecondNameArray();
-		for(int i=0; i< arraySecondName.length-1; i++) {
-			System.out.println(arraySecondName[i]);
-			}
-		String generatePersonIdentity = doGeneatePersonIdentity(arrayFirstName,arrayLastName,arraySecondName);
-		 
-		System.out.println(generatePersonIdentity);
+		String [] arrayFirstName = doGenerateFirstNameArray();		
+		String [] arrayLastName = doGenerateLastNameArray();		
+		String [] arrayPatronymic = doGeneratePatronymicArray();
 		
-		
+		String generatePersonIdentity = doGeneatePersonIdentity(arrayFirstName,arrayLastName,arrayPatronymic);		 
+		System.out.println(generatePersonIdentity);		
 	}
 	private static String[] doGenerateFirstNameArray() {
-		String []  firstNameArray= new String[arrayLength];
+		//TODO: Number of first names should be 10
+		String []  firstNameArray= new String[NUMBER_FIRST_NAME_GENERATED];
 		
 		firstNameArray[0] = "Macclen";
 		firstNameArray[1] = "Loran";
 		firstNameArray[2] = "Gotie";
 		firstNameArray[3] = "Monro";
-		firstNameArray[4] = "Loren";
-				
+		firstNameArray[4] = "Savage";
+		firstNameArray[5] = "Shanel";
+		firstNameArray[6] = "Petro";
+		firstNameArray[7] = "Someonenew";
+		firstNameArray[8] = "Newname";
+		firstNameArray[9] = "Putin";
+					
 		return firstNameArray;
 	}
 	
 	private static String[] doGenerateLastNameArray() {
-		String []  lastNameArray = new String[arrayLength];
+		//TODO: Number of last names should be 7
+		String []  lastNameArray = new String[NUMBER_LAST_NAME_GENERATED];
 		
 		lastNameArray [0] ="Helen";
 		lastNameArray [1] = "Han";
 		lastNameArray [2] = "Janet";
 		lastNameArray [3] = "Ben";
-		lastNameArray [4] = "Luc";
-
+		lastNameArray [4] = "Nino";
+		lastNameArray [5] = "Vanessa";
+		lastNameArray [6] = "Jan";
+				
 		return lastNameArray;
 	}
 	
-	private static String[] doGenerateSecondNameArray() {
-		String []  secondNameArray = new String [arrayLength];
+	private static String[] doGeneratePatronymicArray() {
+		//TODO: Number of patronymics should be 8
+		String []  patronymicsArray = new String [NUMBER_PATRONYMIC_GENERATED];
 		
-		secondNameArray[0] = "Max";
-		secondNameArray[1] = "Oli";
-		secondNameArray[2] = "Rob";
-		secondNameArray[3] = "Mari";
-		secondNameArray[4] = "Peter";
+		patronymicsArray[0] = "Max";
+		patronymicsArray[1] = "Oli";
+		patronymicsArray[2] = "Rob";
+		patronymicsArray[3] = "Mari";
+		patronymicsArray[4] = "Peter";
+		patronymicsArray[5] = "Oliver";
+		patronymicsArray[6] = "Susan";
+		patronymicsArray[7] = "Piter";
 	
-		return secondNameArray;
+		 return patronymicsArray;
 	}
-	private static String doGeneatePersonIdentity(String [] arrayFirstName,String [] arrayLastName,String [] arraySecondName) {
+	private static String doGeneatePersonIdentity(String [] arrayFirstName,String [] arrayLastName,String [] arrayPatronymic) {
 		
 		//3
-		StringBuilder processingLog = new StringBuilder();
+		StringBuilder personsIdentity = new StringBuilder();
 		
-		int randomSumm = 0;		
 		int arrayFirstNameLength = arrayFirstName.length-1;
-		int arrayLasttNameLength = arrayLastName.length-1;
-		int arraySecondtNameLength = arraySecondName.length-1;
+		int arrayLastNameLength = arrayLastName.length-1;
+		int arrayPatronymicLength = arrayPatronymic.length-1;
 		
-		int randomArrayIndexFirstNameItem;
-		int randomArrayIndexLastNameItem;
-		int randomArrayIndexSecondNameItem;
+		int firstNameRandomIndex;
+		int lastNameRandomIndex;
+		int patronymicRandomIndex;
+	
+		//TODO: answer the question: why does Eclipse pay your attention to 3 lines below?
+		//TODO: because of that's unused, what will happen with the lines after build?
+		//HINT: code elimination optimization on Java VM, google it
+		String firstNameRandomItem;
+		String lastNameRandomItem;
+		String patronymicRandomItem;
 	
 	
-		for (int i=0; i <= arrayLength ; i ++ ) {
-			//Make a single calculation!!!
-			randomArrayIndexFirstNameItem = MathHelper.random(0, arrayFirstNameLength);
-			randomArrayIndexLastNameItem = MathHelper.random(0, arrayLasttNameLength);
-			randomArrayIndexSecondNameItem = MathHelper.random(0, arraySecondtNameLength);
+		for (int i=0; i <= NUMBER_PEOPLE_GENERATED ; i ++ ) {
 			
-			processingLog.append("\nfirst name index: ");
-			processingLog.append(randomArrayIndexFirstNameItem);
-			processingLog.append("\nlast name index: ");
-			processingLog.append(randomArrayIndexLastNameItem);
-			processingLog.append("\nsecond name index: ");
-			processingLog.append(randomArrayIndexSecondNameItem);
-			processingLog.append("\nitem: ");
-			//processingLog.append(array2D[randomArrayIndexExternalItem][randomArrayIndexInternalItem]);			
 			
-			//randomSumm += array2D[randomArrayIndexExternalItem][randomArrayIndexInternalItem];
-			/*processingLog.append("\nintermideary amount:");
-			processingLog.append(randomSumm);
-			processingLog.append("\n");	
+			//Generate random number in frames of firstNames array
+			firstNameRandomIndex = MathHelper.random(0, arrayFirstNameLength);
+			//Get randomly first name
+			firstNameRandomItem = arrayFirstName[firstNameRandomIndex];
+			//TODO: retrieve correctly last name and patronymic making comment for each code line 
+			lastNameRandomIndex = MathHelper.random(0, arrayLastNameLength);
+			//Get randomly last name
+			lastNameRandomItem = arrayLastName[lastNameRandomIndex];
+			patronymicRandomIndex = MathHelper.random(0, arrayPatronymicLength);	
+			//Get randomly patronumic
+			patronymicRandomItem = arrayPatronymic[patronymicRandomIndex];
+			personsIdentity.append("\nfirst name: ");
+			personsIdentity.append(firstNameRandomItem);
+			personsIdentity.append("\nlast name: ");
+			personsIdentity.append(lastNameRandomItem);
+			personsIdentity.append("\npatronymic: ");
+			personsIdentity.append(patronymicRandomItem);
+								
 		}
 		
-		processingLog.append("\n\nFinal result:");
-		processingLog.append(randomSumm);*/
-		}
-		return processingLog.toString();
+		return personsIdentity.toString();
 	}
 }
 	
